1dd020518c9fda0fa51bd55a742eee74fa078c6a
Kudafoke isaiah <kudastech@gmail.com>
2025-05-01 15:38:16 +0100

    setup templates files

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    return render(request, 'accounts/dashboard.html')
+
+def product(request):
+    return render(request, "accounts/products.html")
+
+def customer(request):
+    return render( request, "accounts/customer.html")
+
 
 # Create your views here.
