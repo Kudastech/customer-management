1eeb3f7af490b377ffaf46ab63664d39b9defa9d
Kudafoke isaiah <kudastech@gmail.com>
2025-05-02 04:55:02 +0100

    model and migration

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,52 @@
 from django.db import models
 
 # Create your models here.
+
+class Customer(models.Model):
+    name = models.CharField(max_length = 200, null=True)
+    phone = models.CharField(max_length = 200, null=True)
+    email = models.CharField(max_length = 200, null=True)
+    date_created = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return self.name
+
+                
+class Tag(models.Model):
+    name = models.CharField(max_length = 200, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+
+    class Category(models.TextChoices):
+        INDOOR = 'indoor','Indoor'
+        OUTDOOR = 'outdoor', 'Outdoor'
+        ELECTRONICS = 'electronics', 'Electronics'
+
+    name = models.CharField(max_length = 200, null=True)
+    price = models.FloatField(null=True)
+    category = models.CharField(max_length = 200, null=True, choices= Category)
+    description = models.CharField(max_length = 200, null=True, blank= True)
+    date_created = models.DateTimeField(auto_now_add = True)
+    tag = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+
+    class OrderStatus(models.TextChoices):
+        PENDING = 'pending','Pending'
+        OUT_FOR_DELIVERY = 'out_for_delivery', 'Out for Delivery'
+        DELIVERED = 'delivered', 'Delivered'
+ 
+    customer = models.ForeignKey(Customer, null=True, on_delete= models.SET_NULL)
+    product = models.ForeignKey(Product, null=True, on_delete= models.SET_NULL)
+    date_created = models.DateTimeField(auto_now_add = True)
+    status = models.CharField(max_length = 200, null=True, choices = OrderStatus)
+
+
+
